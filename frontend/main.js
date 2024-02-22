da33dd6c57c4187b1f02659343c285f507902bbf
Pranab  Gubhaju <pranabgubhaju@gmail.com>
2024-02-22 13:42:38 +0545

    frontend update

---
+++ frontend/main.js
@@ -0,0 +1,42 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("Scene");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if(section){
+        let position = section.getBoundingClientRect();
+        window.scrollTo(position.left, position.top + window.scrollY - 90);
+
+    }
+}
+
+function gotoeditor(path){
+    window.location.href = path;
+}
